7350dd878658d7183840512c59ac6895267834a1
Ryan Henderson <ryanehenderson@protonmail.com>
2023-04-18 11:01:33 -0500

    Update to handle two text files instead

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
-const { discordToken, channelId, fileName } = require('./config.json');
+const {
+    discordToken,
+    channelId,
+    fileName1,
+    fileName2
+} = require('./config.json');
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
-let questions = [];
+let questions1;
+let questions2;
+let questions;
+let channel;
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
-    fs.promises.readFile(`./${fileName}`, 'utf8').then((data) => {
-        questions = data.split('\n');
-        postQuestion();
-        setInterval(() => {
+    channel = client.channels.cache.get(channelId);
+    fs.promises.readFile(`./${fileName1}`, 'utf8').then((data) => {
+        questions1 = data.split('\n');
+        fs.promises.readFile(`./${fileName2}`, 'utf8').then((otherData) => {
+            questions2 = otherData.split('\n');
+            questions = questions1.concat(questions2);
             postQuestion();
-        }, 1000 * 60 * 60 * 4);
+            setInterval(() => {
+                postQuestion();
+            }, 1000 * 60 * 60 * 4);
+        });
     });
 });
 
 function postQuestion() {
     let randomQuestion =
         questions[Math.floor(Math.random() * questions.length)];
-    let channel = client.channels.cache.get(channelId);
     channel.send(randomQuestion);
 }
 
